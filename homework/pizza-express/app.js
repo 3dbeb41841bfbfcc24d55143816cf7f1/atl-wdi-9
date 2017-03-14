5ad562438f9c0f3f1d3b6c92dfbfad9f2c7bd765
michael <michael.atorres90@gmail.com>
2017-03-14 17:32:26 -0400

    started hw

---
+++ homework/pizza-express/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app     = express();
+var port    = 3000;
+
+// tells the server to listen for requests on port 3000
+app.listen(port, function(){
+  console.log("==========================")
+  console.log('LISTENING ON PORT ' + port);
+  console.log("==========================")
+});
