104c00e02e43a9a0201c394dca96ffe479addcfd
michael <michael.atorres90@gmail.com>
2017-03-14 19:20:12 -0400

    rafactored routes and added views

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -8,12 +8,18 @@ app.set('views', './views');
 
 
 app.get('/', function(req, res) {
-		res.send("Welcome to Pizza Express!");
+		//res.send("Welcome to Pizza Express!");
+		res.render('index.hbs', {
+			data: "Welcome to pizza Express"
+		});
 });
 
 app.get('/topping/:type', function(req, res, next) {
 
-    res.send(`${req.params.type} Pizza, Good choice!`);
+    //res.send(`${req.params.type} Pizza, Good choice!`);
+    res.render('toppings.hbs', {
+    	data: req.params.type + " " + " "
+    });
 });
 
 
@@ -21,4 +27,12 @@ app.listen(port, function(){
   console.log("==========================")
   console.log('LISTENING ON PORT ' + port);
   console.log("==========================")
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+
+	//res.send(`order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+	res.render('order.hbs', {
+		data: req.params.amount + req.params.size + " " + " "
+	});
 });
