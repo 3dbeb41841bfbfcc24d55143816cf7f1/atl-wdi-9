b11a025cbb7a2bc9275466030ea89559ea11488b
michael <michael.atorres90@gmail.com>
2017-03-14 17:58:22 -0400

    completed Get routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app     = express();
+var hbs     = require('hbs');
 var port    = 3000;
+
+
+app.get('/', function(req, res) {
+		res.send("Welcome to Pizza Express!");
+});
+
+app.get('/topping/:type', function(req, res, next) {
+
+    res.send(`${req.params.type} Pizza, Good choice!`);
+});
+
 
 app.listen(port, function(){
   console.log("==========================")
