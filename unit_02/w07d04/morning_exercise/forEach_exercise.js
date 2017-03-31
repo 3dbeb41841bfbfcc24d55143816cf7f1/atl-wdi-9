b07c8ae8e66076539f0dffd5315d9d294935c5f9
michael <michael.atorres90@gmail.com>
2017-03-31 14:13:23 -0400

    updated all

---
+++ unit_02/w07d04/morning_exercise/forEach_exercise.js
@@ -0,0 +1,5 @@
+
+ for(int i = 0; i < array2.length; i++){
+    array2[i] = array2[i].toUpperCase();
+}
+
--- unit_03/w09d01/student_labs/giphy_app_starter/public/scripts/app.js
+++ unit_03/w09d01/student_labs/giphy_app_starter/public/scripts/app.js
@@ -1,3 +1,14 @@
 $(function() {
 
+$('.get-gif').on("click", function() {
+  $.get('http://localhost:3000/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg')
+  	.done(function(giphyResponse) {
+  		console.log(data.data.image_rul);
+  	})
+  		$('.image-jumbotron').attr('src', data.data.image_url);
+  		.fail(function(data) {
+  			console.log(data.data.errorMessage);
+  		});
+ })
+
 })
